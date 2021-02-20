ab913706a3bf249b48b25bf521161561878b287b
Rony Allaw <rga45@mail.aub.edu>
2021-02-21 01:37:54 +0200

    fixed stupid git merge

--- PC App/Windows_Version/Diagnostic_Data/Disk_Windows/DiskInfo.cs
+++ PC App/Windows_Version/Diagnostic_Data/Disk_Windows/DiskInfo.cs
@@ -4,86 +4,43 @@ using System.IO;
 
 namespace Disk_Windows
 {
-    /// <summary>
-    /// Class responsible for disk info of PCs' runnung on Windows OS
-    /// </summary>
     public class DiskInfo
     {
-        /// <summary>
-        /// Creates a performanceCounter object indicating how much of the disk's read/write capability is used
-        /// </summary>
-        private PerformanceCounter diskCounter;
+        private PerformanceCounter diskCounter;//creates a performanceCounter object indicating how much of the disk's read/write capability is used
 
-        
-        
         public PerformanceCounter DiskCounter { get => diskCounter; set => diskCounter = value; }
-        
-        
-        /// <summary>
-        /// Stores the percentage of the used disk storage
-        /// </summary>
-        private double diskUsagePercentage;
+        public float DiskCounterPercentage { get => diskCounterPercentage; set => diskCounterPercentage = value; }
+        public long FreeSpaceInGB { get => freespaceinGB; set => freespaceinGB = value; }
 
-        /// <summary>
-        /// Getter for Disk Usage Percentage
-        /// </summary>
-        /// <returns>Gets updated Disk Usage Percentage</returns>
-        public double DiskUsagePercentage { get => updateDiskUsage(); }
-        
-        
-        
-        /// <summary>
-        /// Stores how much disk storage is free in GB
-        /// </summary>
-        private double diskFreeSpaceInGB;
-        
-        public double DiskFreeSpaceInGB { get => diskFreeSpaceInGB; set => diskFreeSpaceInGB = value; }
-        
-
-
-
-        /// <summary>
-        /// Default constructor that tells the class to get the disk information
-        /// </summary>
+        private float diskCounterPercentage;
+        private long freespaceinGB;
         public DiskInfo()
         {
             diskCounter = new PerformanceCounter();
             DiskCounter.CategoryName = "PhysicalDisk";
             DiskCounter.CounterName = "% Disk Time";
             DiskCounter.InstanceName = "_Total";
+            DiskCounterPercentage = updateDiskUsage();
         }
         
-        
-        
-        /// <summary>
-        /// Updates DiskFreeSpaceInGB the available space in all the disks
-        /// </summary>
-        /// <returns>Returns the available space in all disks</returns>
-        public double updateFreeSpaceInGB()
+        public long updateFreeSpaceInGB()//updates and returns FreeSpaceInGB the available free space in all the disks
         {
             foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
                 if (drive.IsReady)
                 {
-                    DiskFreeSpaceInGB = drive.AvailableFreeSpace / 1000000000.0;
+                    FreeSpaceInGB = drive.AvailableFreeSpace / 1000000000;
                 }
             }
-            return DiskFreeSpaceInGB;
+            return FreeSpaceInGB;
         }
-        
-        
-        
-        
-        /// <summary>
-        /// Updates DiskounterPercentage to the current used percentage. It starts from zero then the system rests for 1 sec then updates the percentage to its true value.
-        /// </summary>
-        /// <returns>Returns the current percentage of the used disk storage</returns>
-        public double updateDiskUsage()//updates and returns DiskUsagePercentage to get the latest
+
+        public float updateDiskUsage()//updates and returns DiskCounterPercentage to get the latest
         {
-            diskUsagePercentage = DiskCounter.NextValue();
+            DiskCounterPercentage = DiskCounter.NextValue(); //always starts at 0
             System.Threading.Thread.Sleep(1000);
-            diskUsagePercentage = DiskCounter.NextValue();// now matches task manager value
-            return diskUsagePercentage;
+            DiskCounterPercentage = DiskCounter.NextValue();// now matches task manager value
+            return DiskCounterPercentage;
         }
 
     }
