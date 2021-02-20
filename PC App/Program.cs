bde1a80964a5cf61d80f6a25dbefe1d882001dad
Rony Allaw <rga45@mail.aub.edu>
2021-02-21 00:31:44 +0200

    Reverting to commit 4e86ff184bcc0530e3f113ab835bf2fdef8e635b

--- PC App/Program.cs
+++ PC App/Program.cs
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-﻿using Serialization;
-using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-=======
 ﻿using System;
 using Disk_Linux;
 
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b
 namespace PC_App
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-
-=======
             var diskInfo = new DiskInfo();
             Console.WriteLine();
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b
         }
     }
 }
--- PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
+++ PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
@@ -4,12 +4,6 @@ namespace Network_Windows
 {
     public class NetworkInfo
     {
-<<<<<<< HEAD:PC App/Windows_Version/Diagnostic_Data/Network/NetworkInfo.cs
-        private PerformanceCounter bandwidthCounter = new PerformanceCounter(); //creates a performance counter for network interface to monitor current bandwith
-        private float bandwidth;//sets bandwith value to 0
-        private PerformanceCounter dataSentCounter = new PerformanceCounter(); //creates a performance counter for network interface to monitor data sent
-        private PerformanceCounter dataReceivedCounter = new PerformanceCounter();//creates a performance counter for network interface to monitor data received
-=======
         /// <summary>
         /// Creates a performance Counter object for network interface to monitor current bandwith
         /// </summary>
@@ -29,58 +23,47 @@ namespace Network_Windows
         /// <summary>
         /// A variable to store the percentage of network traffic
         /// </summary>
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b:PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
         private double NetworkTrafficPercentage;
+        private String[] models;
         public PerformanceCounter BandwidthCounter { get => bandwidthCounter; set => bandwidthCounter = value; }
         public float Bandwidth { get => bandwidth; set => bandwidth = value; }
         public PerformanceCounter DataSentCounter { get => dataSentCounter; set => dataSentCounter = value; }
         public PerformanceCounter DataReceivedCounter { get => dataReceivedCounter; set => dataReceivedCounter = value; }
+        public string[] Models { get => models; set => models = value; }
+
         public NetworkInfo()
         {
-            bandwidthCounter.CategoryName = "Network Interface";
-            bandwidthCounter.CounterName = "Current Bandwidth";
+            bandwidthCounter = new PerformanceCounter();
+            bandwidth = BandwidthCounter.NextValue(); ;
+            NetworkTrafficPercentage = updateNetworkUsage();
+            dataSentCounter = new PerformanceCounter();
+            dataReceivedCounter = new PerformanceCounter();
+            BandwidthCounter.CategoryName = "Network Interface";
+            BandwidthCounter.CounterName = "Current Bandwith";
             DataSentCounter.CategoryName = "Network Interface";
             DataSentCounter.CounterName = "Bytes Sent/sec";
             DataReceivedCounter.CategoryName = "Network Interface";
             DataReceivedCounter.CounterName = "Bytes Received/sec";
-            
-            NetworkTrafficPercentage = updateNetworkUsage();
-            
+            PerformanceCounterCategory category = new PerformanceCounterCategory("Network Interface"); //creates a category object representing the network interface
+            Models = category.GetInstanceNames(); //array that has the models of network devices
         }
 
-<<<<<<< HEAD:PC App/Windows_Version/Diagnostic_Data/Network/NetworkInfo.cs
-        public double updateNetworkUsage() //prints a percentage showing network traffic
-=======
         /// <summary>
         /// Prints network traffic percentage
         /// Contains a for loop to set the model for each counter and print the network usage for each model
         /// </summary>
         /// <returns></returns>
         public double updateNetworkUsage() 
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b:PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
         {
-            PerformanceCounterCategory category = new PerformanceCounterCategory("Network Interface"); //creates a category object representing the network interface
-            String[] models = new String[category.GetInstanceNames().Length]; //array that has the models of network devices
-            models = category.GetInstanceNames();
 
-<<<<<<< HEAD:PC App/Windows_Version/Diagnostic_Data/Network/NetworkInfo.cs
-            foreach (String model in models) //this for loop sets the model for each counter and prints the network usage for each model
-=======
             foreach (String model in Models) 
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b:PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
             {
                 BandwidthCounter.InstanceName = model;
                 Bandwidth = BandwidthCounter.NextValue(); 
                 DataSentCounter.InstanceName = model;
                 DataReceivedCounter.InstanceName = model;
-<<<<<<< HEAD:PC App/Windows_Version/Diagnostic_Data/Network/NetworkInfo.cs
-                float datasent = DataSentCounter.NextValue(); //sets datasent to datasentCounter's value
-                float datareceived = DataReceivedCounter.NextValue(); //sets data received to dataReceived's value
-                bandwidth = bandwidthCounter.NextValue();
-=======
                 float datasent = DataSentCounter.NextValue(); 
                 float datareceived = DataReceivedCounter.NextValue();
->>>>>>> 4e86ff184bcc0530e3f113ab835bf2fdef8e635b:PC App/Windows_Version/Diagnostic_Data/Network_Windows/NetworkInfo.cs
                 if (Bandwidth != 0)
                 {
                     NetworkTrafficPercentage = (8 * (datasent + datareceived)) / Bandwidth * 100;
