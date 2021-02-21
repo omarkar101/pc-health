8911c1c78164e2d84eccc7d0cffcc578fd461475
Omar Karazoun <59054172+omarkar101@users.noreply.github.com>
2021-02-21 19:12:46 +0200

    Update NetworkInfo.cs

--- PC App/Linux_Version/Diagnostic_Data/Network_Linux/NetworkInfo.cs
+++ PC App/Linux_Version/Diagnostic_Data/Network_Linux/NetworkInfo.cs
@@ -11,19 +11,16 @@ namespace PC_App.Linux_Version.Diagnostic_Data.Network_Linux
         /// Stores network Bytes Sent
         /// </summary>
         private double _avgNetworkBytesSent;
-        private double _networkBytesSent;
 
         /// <summary>
         /// Getter for network Bytes Sent
         /// </summary>
         public double AvgNetworkBytesSent => UpdateNetworkInfo().AvgBytesSent;
-        public double NetworkBytesSent => UpdateNetworkInfo().BytesSent;
 
         /// <summary>
         /// Stores network Bytes Received 
         /// </summary>
         private double _avgNetworkBytesReceived;
-        private double _networkBytesReceived;
 
         /// <summary>
         /// Getter for network Bytes Received
@@ -32,8 +29,6 @@ namespace PC_App.Linux_Version.Diagnostic_Data.Network_Linux
         
         
         
-        public double NetworkBytesReceived => UpdateNetworkInfo().BytesReceived;
-
         /// <summary>
         /// Constructor which updates the Network Info
         /// </summary>
@@ -58,17 +53,6 @@ namespace PC_App.Linux_Version.Diagnostic_Data.Network_Linux
             _avgNetworkBytesReceived = bytesReceivedCounter / (interfaces.Length - 1);
             _avgNetworkBytesSent = bytesSentCounter / (interfaces.Length - 1);
             return (_avgNetworkBytesSent, _avgNetworkBytesReceived);
-
-        private (double BytesSent, double BytesReceived) UpdateNetworkInfo()
-        {
-            if (!NetworkInterface.GetIsNetworkAvailable()) return (0, 0);
-            var interfaces = NetworkInterface.GetAllNetworkInterfaces();
-            foreach(var ni in interfaces)
-            {
-                _networkBytesReceived = ni.GetIPv4Statistics().BytesReceived;
-                _networkBytesSent = ni.GetIPv4Statistics().BytesSent;
-            }
-            return (_networkBytesSent, _networkBytesReceived);
         }
         
             
