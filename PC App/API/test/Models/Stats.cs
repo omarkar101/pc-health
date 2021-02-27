02991ebc36476590d3ac0e7db350afced7e8af7b
Rony Allaw <rga45@mail.aub.edu>
2021-02-27 16:58:19 +0200

    added API service

---
+++ PC App/API/test/Models/Stats.cs
@@ -0,0 +1,12 @@
+namespace test{
+    public class stats{
+        public float _cpuUsage{get;set;}
+
+        public float _totalFreeDiskSpace{get;set;}
+
+        public float _diskUsage{get;set;}
+
+        public double _memoryUsage{get;set;}
+
+    }
+}
