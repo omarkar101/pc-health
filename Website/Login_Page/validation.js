ce4673aced3d329737c5231e496638b03f045680
Mahdi Mattout <mahdimattout04@gmail.com>
2021-03-05 14:31:53 +0200

    Documented JavaScriptCode For Home and Login Pages

--- Website/Login_Page/validation.js
+++ Website/Login_Page/validation.js
@@ -1,3 +1,7 @@
+/**
+ * This functions take input the login username and password of an account and gives/rejects access to the home page. If access was given, it automatically redirects the user to the home page.
+ * @returns bool indicating if the user gets access or not
+ */
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
