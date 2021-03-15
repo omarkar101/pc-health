7b83573c602dcd3f2474adc5d3207d33273e9bed
Omar Karazoun <omar.kar101@gmail.com>
2021-03-15 15:56:02 +0200

    .

--- API/WebApi/Controllers/BaseController.cs
+++ API/WebApi/Controllers/BaseController.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Services;
-using System.Web.Http.Cors;
-using Newtonsoft.Json;
 using WebApi.Models;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
@@ -17,25 +11,25 @@ namespace WebApi.Controllers
     public class BaseController : ControllerBase
     {
         [HttpGet]
-        public string DiagnosticData()
+        public string GetDiagnosticData()
         {
-            var lstValueCollection = StaticStorageServices.PC_Mapper.Values;
-            return JsonSerializer.Serialize(lstValueCollection);
+            var PCsList = StaticStorageServices.PC_Mapper.Values;
+            return JsonSerializer.Serialize(PCsList);
         }
 
         [HttpGet]
         public string GetTime()
         {
-            var time = DateTime.UtcNow;
-            if ((time - StaticStorageServices.TimeCurrent).TotalMilliseconds >= 0)
+            var timeNowUtc = DateTime.UtcNow;
+            if ((timeNowUtc - StaticStorageServices.TimeToGetPcConfiguration).TotalMilliseconds >= 0)
             {
-                StaticStorageServices.TimeCurrent = time.AddSeconds(StaticStorageServices.TimeToAdd.Time);
+                StaticStorageServices.TimeToGetPcConfiguration = timeNowUtc.AddSeconds(StaticStorageServices.PCsConfiguration.Time);
             }
-            return JsonSerializer.Serialize(StaticStorageServices.TimeCurrent);
+            return JsonSerializer.Serialize(StaticStorageServices.TimeToGetPcConfiguration);
         }
 
         [HttpPost]
-        public void GetDiagnosticDataFromPc(Diagnostic_Data diagnosticData)
+        public void PostDiagnosticDataFromPc(Diagnostic_Data diagnosticData)
         {
             if (StaticStorageServices.PC_Mapper.ContainsKey(diagnosticData.PC_ID))
             {
@@ -47,9 +41,8 @@ namespace WebApi.Controllers
         [HttpPost]
         public void PostTimeConfiguration(ConfigurationFromWebsiteData configuration)
         {
-            StaticStorageServices.TimeToAdd = configuration;
-            StaticStorageServices.TimeCurrent = DateTime.UtcNow.AddSeconds(StaticStorageServices.TimeToAdd.Time);
-            // Console.WriteLine(configuration.Time);
+            StaticStorageServices.PCsConfiguration = configuration;
+            StaticStorageServices.TimeToGetPcConfiguration = DateTime.UtcNow.AddSeconds(StaticStorageServices.PCsConfiguration.Time);
         }
     }
 }
