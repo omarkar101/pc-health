242b1777a43705a5828a0578e7fb840a7d081221
omarkar101 <omar.kar101@gmail.com>
2021-04-15 18:32:00 +0300

    Cleaned Pc App Code

--- PC App/Services/DiagnosticDataServices.cs
+++ PC App/Services/DiagnosticDataServices.cs
@@ -38,77 +38,98 @@ namespace PC_App.Services
 
             var pcConfigurations = JsonSerializer.Deserialize<PcConfiguration>(pcConfigurationJsonString);
 
+            if (pcConfigurations == null) return "false";
+            //making to lower
+            MakePcConfigurationToLower(pcConfigurations);
+
+            var diagnosticData = CreateDiagnosticData(pcConfigurations);
+
+            CountHigh(diagnosticData);
+
+            if (DateTime.UtcNow.CompareTo(StallTime) >= 0 && StallTime.CompareTo(DateTime.MinValue) != 0)
+            {
+                StallTime = DateTime.MinValue;
+            }
+
+            await SendWarningIfDangerousAndNoStall(diagnosticData);
+
+            return JsonSerializer.Serialize(diagnosticData);
+
+        }
+
+        private static void MakePcConfigurationToLower(PcConfiguration? pcConfigurations)
+        {
             if (pcConfigurations != null)
             {
-                //making to lower
                 pcConfigurations.PcEmail = pcConfigurations.PcEmail.ToLower();
                 for (var i = 0; i < pcConfigurations.Admins.Count; i++)
                 {
-                    pcConfigurations.Admins[i] = new Tuple<string, string>(pcConfigurations.Admins[i].Item1.ToLower(), pcConfigurations.Admins[i].Item2);
+                    pcConfigurations.Admins[i] = new Tuple<string, string>(pcConfigurations.Admins[i].Item1.ToLower(),
+                        pcConfigurations.Admins[i].Item2);
                 }
-
-                var linuxFalseWindowsTrue = !RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-
-                var diagnosticData = new DiagnosticData()
-                {
-                    CpuUsage =
-                        (linuxFalseWindowsTrue ? Cpu_Windows.CpuInfo.CpuPercentage : Cpu_Linux.CpuInfo.CpuPercentage),
-                    AvgNetworkBytesReceived = NetworkInfo.AvgNetworkBytesReceived,
-                    AvgNetworkBytesSent = NetworkInfo.AvgNetworkBytesSent,
-                    DiskTotalSpace = DiskInfo.DiskCounterPercentage,
-                    MemoryUsage = (linuxFalseWindowsTrue
-                        ? Memory_Windows.MemoryInfo.RamUsagePercentage
-                        : Memory_Linux.MemoryInfo.MemoryUsagePercentage),
-                    TotalFreeDiskSpace = DiskInfo.FreeSpaceInGb,
-                    PcId = new DeviceIdBuilder()
-                        .AddMachineName()
-                        .ToString(),
-                    Os = (linuxFalseWindowsTrue ? "Windows" : "Linux"),
-                    Services = linuxFalseWindowsTrue
-                        ? PC_App.Windows_Version.Diagnostic_Data.Services_Windows.ServicesInfo.ServicesNamesAndStatus
-                        : PC_App.Linux_Version.Diagnostic_Data.Services_Linux.ServicesInfo.ServicesNamesAndStatus,
-                    FirewallStatus = linuxFalseWindowsTrue
-                        ? (PC_App.Windows_Version.Diagnostic_Data.Firewall_Windows.FirewallInfo.FirewallStatus
-                            ? "Active"
-                            : "Inactive")
-                        : PC_App.Linux_Version.Diagnostic_Data.Firewall_Linux.FirewallInfo.FirewallStatus
-                            ? "Active"
-                            : "Inactive",
-                    CurrentSecond = (Counter = (Counter + 1) % 60),
-                    PcConfiguration = pcConfigurations,
-                    HealthStatus = "Healthy"
-                };
-
-                CountHigh(diagnosticData);
-
-                if (DateTime.UtcNow.CompareTo(StallTime) >= 0 && StallTime.CompareTo(DateTime.MinValue) != 0)
-                {
-                    StallTime = DateTime.MinValue;
-                }
-
-                if (CpuHighCounter >= 40 || MemoryHighCounter >= 40)
-                {
-                    diagnosticData.HealthStatus = "Unhealthy";
-                    if (StallTime.CompareTo(DateTime.MinValue) == 0)
-                    {
-                        var pcHealthData = new PcHealthData()
-                        {
-                            CpuHighCounter = CpuHighCounter,
-                            MemoryHighCounter = MemoryHighCounter,
-                            PcConfiguration = diagnosticData.PcConfiguration ?? new PcConfiguration()
-                        };
-                        StallTime = DateTime.UtcNow.AddMinutes(10);
-                        await PostServices.PostPcHealthData("https://localhost:44335/Pc/PostPcHealthDataFromPc",
-                            pcHealthData);
-                    }
-                }
-
-                return JsonSerializer.Serialize(diagnosticData);
             }
-
-            return "false";
         }
 
+        private static DiagnosticData CreateDiagnosticData(PcConfiguration? pcConfigurations)
+        {
+            var linuxFalseWindowsTrue = !RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+            var diagnosticData = new DiagnosticData()
+            {
+                CpuUsage =
+                    (linuxFalseWindowsTrue ? Cpu_Windows.CpuInfo.CpuPercentage : Cpu_Linux.CpuInfo.CpuPercentage),
+                AvgNetworkBytesReceived = NetworkInfo.AvgNetworkBytesReceived,
+                AvgNetworkBytesSent = NetworkInfo.AvgNetworkBytesSent,
+                DiskTotalSpace = DiskInfo.DiskCounterPercentage,
+                MemoryUsage = (linuxFalseWindowsTrue
+                    ? Memory_Windows.MemoryInfo.RamUsagePercentage
+                    : Memory_Linux.MemoryInfo.MemoryUsagePercentage),
+                TotalFreeDiskSpace = DiskInfo.FreeSpaceInGb,
+                PcId = new DeviceIdBuilder()
+                    .AddMachineName()
+                    .ToString(),
+                Os = (linuxFalseWindowsTrue ? "Windows" : "Linux"),
+                Services = linuxFalseWindowsTrue
+                    ? PC_App.Windows_Version.Diagnostic_Data.Services_Windows.ServicesInfo.ServicesNamesAndStatus
+                    : PC_App.Linux_Version.Diagnostic_Data.Services_Linux.ServicesInfo.ServicesNamesAndStatus,
+                FirewallStatus = linuxFalseWindowsTrue
+                    ? (PC_App.Windows_Version.Diagnostic_Data.Firewall_Windows.FirewallInfo.FirewallStatus
+                        ? "Active"
+                        : "Inactive")
+                    : PC_App.Linux_Version.Diagnostic_Data.Firewall_Linux.FirewallInfo.FirewallStatus
+                        ? "Active"
+                        : "Inactive",
+                CurrentSecond = (Counter = (Counter + 1) % 60),
+                PcConfiguration = pcConfigurations,
+                HealthStatus = "Healthy"
+            };
+            return diagnosticData;
+        }
+
+        private static async Task SendWarningIfDangerousAndNoStall(DiagnosticData diagnosticData)
+        {
+            if (CpuHighCounter >= 40 || MemoryHighCounter >= 40)
+            {
+                diagnosticData.HealthStatus = "Unhealthy";
+                if (StallTime.CompareTo(DateTime.MinValue) == 0)
+                {
+                    await SendWarning(diagnosticData.PcConfiguration);
+                }
+            }
+        }
+
+        private static async Task SendWarning(PcConfiguration pcConfiguration)
+        {
+            var pcHealthData = new PcHealthData()
+            {
+                CpuHighCounter = CpuHighCounter,
+                MemoryHighCounter = MemoryHighCounter,
+                PcConfiguration = pcConfiguration ?? new PcConfiguration()
+            };
+            StallTime = DateTime.UtcNow.AddMinutes(10);
+            await PostServices.PostPcHealthData("https://localhost:44335/Pc/PostPcHealthDataFromPc",
+                pcHealthData);
+        }
         private static void CountHigh(DiagnosticData diagnosticData)
         {
             if (_cpuHighBitArray[Counter])
