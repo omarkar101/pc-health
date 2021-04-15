3ea576ae7fd45f3b111e491f3f353ef6b1cdde80
omarkar101 <omar.kar101@gmail.com>
2021-04-15 18:48:31 +0300

    Fixed CpuInfo

--- PC App/Windows_Version/Diagnostic_Data/Cpu_Windows/CpuInfo.cs
+++ PC App/Windows_Version/Diagnostic_Data/Cpu_Windows/CpuInfo.cs
@@ -12,14 +12,15 @@ namespace Cpu_Windows
         private static float UpdateCpuUsage()//returns CpuPercentage
         {
             PerformanceCounter cpuCounter = new PerformanceCounter();//creates a Performance Counter object that indicates how much processing power is used
-            cpuCounter.CategoryName = "Processor";
-            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.CategoryName = "Processor Information";
+            cpuCounter.CounterName = "% Processor Utility";
             cpuCounter.InstanceName = "_Total";
-            float firstValue = cpuCounter.NextValue();
-            System.Threading.Thread.Sleep(500);
+            var firstValue = cpuCounter.NextSample();
+            System.Threading.Thread.Sleep(100);
             // now matches task manager reading
-            float currentValue = cpuCounter.NextValue();
-            return currentValue;
+            var currentValue = cpuCounter.NextSample();
+            var cpuValue = CounterSample.Calculate(firstValue, currentValue);
+            return cpuValue;
         }
     }
 }
