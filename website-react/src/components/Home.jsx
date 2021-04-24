bc5d8b2be24074f42d45da49514f4d85d5ba609f
Mahdi Mattout <mahdimattout04@gmail.com>
2021-04-25 02:10:42 +0300

    Added Home and AboutUs

---
+++ website-react/src/components/Home.jsx
@@ -0,0 +1,252 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+function Home() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" aria-disabled="true">
+            PC-Health
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="true"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/aboutus">
+                  About Us
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="/images/Diagram.png"></img>
+            <div className="container">
+              <div className="carousel-caption text-start">
+                <h1>Welcome to PC-Health</h1>
+                <p style={{ color: "black" }}>
+                  The Ultimate website to monitor to easily monitor the health
+                  of your company's PCs'
+                </p>
+                <p>
+                  <Link className="btn btn-lg btn-primary" to="/register">
+                    Sign up today
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="featurette-divider" />
+      <div className="container marketing">
+        <div className="row">
+          <div className="col-lg-4">
+            <svg
+              className="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2>
+              <br />
+              Monitor an unlimited number of PCs all in one place
+            </h2>
+          </div>
+          <div className="col-lg-4">
+            <svg
+              className="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+            <h2>
+              <br/>
+              Get notified immediately about any PC in danger
+            </h2>
+          </div>
+          <div className="col-lg-4">
+            <svg
+              className="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2>
+              <br /> View real-time detailed information
+            </h2>
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+        <h2>Steps to install and configure the application:</h2>
+        <br />
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h2 className="featurette-heading">
+              Step 1: Installation Process <br />
+            </h2>
+            <p className="lead">
+              First, sign up on our website using your email. Next, install the application using the setup that can be found
+              <Link>Here</Link>
+            </p>
+            <p>
+              Note: We <b>recommend</b> installing the application in any
+              directory outside <b>Program Files</b>. Installing the application
+              in "Program Files" will require you to configure the configuration
+              file of the application manually.
+            </p>
+          </div>
+          <div className="col-md-5">
+            <svg
+              className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+              width="500"
+              height="500"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x500"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee" />
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x500
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+
+        <div className="row featurette">
+          <div className="col-md-7 order-md-2">
+            <h2 className="featurette-heading">Step 2: Configuring the PC</h2>
+            <p className="lead">
+              After Installing the application, open the folder, and click on
+              PC-Configuration.jar and insert the PC's name and contact info and
+              assign an admin email (email used in Step 1) to it. After that, click Apply. After
+              configuring all the PCs, run "PC App.exe"
+            </p>
+          </div>
+          <div className="col-md-5 order-md-1">
+            <svg
+              className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+              width="500"
+              height="500"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x500"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee" />
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x500
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h2 className="featurette-heading">Step 3:</h2>
+            <p className="lead"> After finishing configuring the PCs, log in to the website, and you will be able to see real-time statistics of all configured PCs.</p>
+          </div>
+          <div className="col-md-5">
+            <svg
+              className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+              width="500"
+              height="500"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x500"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee" />
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x500
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+      </div>
+
+      <footer className="container">
+        <p className="float-end">
+          <a href="#">Back to top</a>
+        </p>
+        <p>
+          &copy; 2021 CMPS253, Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
