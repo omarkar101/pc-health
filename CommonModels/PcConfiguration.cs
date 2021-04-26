09b758265d49487d70d0021491e1d2bd99b13b6a
omarkar101 <omar.kar101@gmail.com>
2021-04-26 23:32:40 +0300

    Fixed CommonModels

---
+++ CommonModels/PcConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModels
+{
+    public class PcConfiguration
+    {
+        public string PcUsername { get; set; }
+        public string PcEmail { get; set; }
+        public List<Tuple<string, string>> Admins { get; set; }
+    }
+}
---
+++ CommonModels/PcHealthData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModels
+{
+    public class PcHealthData
+    {
+        public PcConfiguration PcConfiguration { get; set; }
+        public int CpuHighCounter { get; set; }
+        public int MemoryHighCounter { get; set; }
+    }
+}
